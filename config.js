f311f2705e595c22263277ee317b3d2d545b98f3
PETER <petersuccessful73@gmail.com>
2025-10-13 12:21:24 +0100

    Update config.js for Ffddff

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const defaults = {
 
 let cache = {}
 
-const SESSION_ID = process.env.SESSION_ID || ''
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=ESZV1637MR'
 cache.SESSION_ID = SESSION_ID 
 
 async function initConfig() {
