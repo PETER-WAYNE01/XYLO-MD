0aaee8de31804ccfd19d84b4173dfc49df668462
PETER <petersuccessful73@gmail.com>
2025-11-07 20:21:20 +0100

    Update config.js for Favour

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const defaults = {
 
 let cache = {}
 
-const SESSION_ID = process.env.SESSION_ID || ''
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=B9TL8WD58Y'
 cache.SESSION_ID = SESSION_ID 
 
 async function initConfig() {
